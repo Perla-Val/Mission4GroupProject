e7daf047fea9a4b57a9f15ed3177fc2b5a5e1505
Perla Valdovinos <95377852+Perla-Val@users.noreply.github.com>
2024-01-31 08:43:03 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿internal class Program
+{
+    private static void Main(string[] args) //nothin should be calling the main class
+                                            //(that is why it is private, you can also have public and protected
+                                            //STATIC means it can stick you can have it unstick
+                                            //VOID explains what is going to be returned in this case nothing but it could say string
+                                            //(Inside the Parenthesis it has what is being passed in this case you have a
+                                            //string array names args) 
+    {
+        System.Console.WriteLine("Hello, World!");
+    }
+}
